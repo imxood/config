585785ac049cf261f7be3e165435c469d148a6ff
imxood <imxood@gmail.com>
2019-01-13 20:44:04 +0800

    .

--- installs.sh
+++ installs.sh
@@ -5,7 +5,7 @@ set -e
 sudo apt autoremove -y firefox thunderbird
 
 # tools
-sudo apt install -y vim chromium-browser git make cmake openjdk-8-jdk openocd supervisor python3 python3-pip curl polipo shadowsocks net-tools fcitx-googlepinyin lftp xclip aria2 fish
+sudo apt install -y vim chromium-browser git make cmake openjdk-8-jdk openocd supervisor python3 python3-pip curl net-tools fcitx-googlepinyin lftp xclip aria2 texi2html
 
 # swap tool
 sudo apt install -y dphys-swapfile
@@ -18,6 +18,9 @@ sudo apt install -y language-pack-zh-han*
 
 # chinese font
 sudo apt install -y ttf-wqy-zenhei
+
+# iptables tool
+sudo apt install -y iptables-persistent
 
 # aria2
 sudo apt install -y aria2
@@ -35,12 +38,35 @@ pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 sudo apt install global libncurses5-dev libncursesw5-dev -y
 
 # web proxy
-if [ ! -f "/etc/polipo/config" ]; then
-	sudo wget --show-progress https://raw.githubusercontent.com/imxood/config/master/etc/polipo/config -N -P /etc/polipo/
-	sudo service polipo restart
+sudo -H pip3 install mitmproxy
+
+/usr/local/bin/mitmproxy
+
+openssl x509 -in mitmproxy-ca-cert.pem -inform PEM -out mitmproxy-ca-cert.crt
+
+sudo mkdir -p /usr/share/ca-certificates/extra
+
+sudo cp mitmproxy-ca-cert.crt /usr/share/ca-certificates/extra/
+
+# sudo dpkg --configure -a
+
+sudo dpkg-reconfigure ca-certificates
+
+sudo apt install -y texinfo
+if [ -z "$(which polipo)" ]; then
+	git clone https://github.com/jech/polipo.git
+	cd polipo && make -j4 && sudo make install
+fi
+sudo apt install -y polipo
+sudo service polipo restart
+
+if [ ! -f "/etc/polipo.config" ]; then
+	sudo wget --show-progress https://raw.githubusercontent.com/imxood/config/master/etc/polipo.config -N -P /etc/
+	sudo wget --show-progress https://raw.githubusercontent.com/imxood/config/master/etc/supervisor/conf.d/polipo.conf -N -P /etc/supervisor/conf.d/
 fi
 
 # socket5 proxy
+sudo apt install -y shadowsocks
 if [ ! -f "/etc/supervisor/conf.d/ss-client.conf" ]; then
         sudo wget --show-progress https://raw.githubusercontent.com/imxood/config/master/etc/supervisor/conf.d/ss-client.conf -N -P /etc/supervisor/conf.d/
 fi
@@ -50,6 +76,23 @@ if [ ! -f "/etc/ss-client.json" ]; then
 fi
 
 sudo supervisorctl reload
+
+# install docker
+curl -sSL https://get.daocloud.io/docker | sh
+# after the opt, reboot
+sudo usermod -aG docker imxood
+
+# docker mirror
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+sudo systemctl restart docker.service
+
+curl -L https://get.daocloud.io/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` > docker-compose
+sudo mv docker-compose /usr/local/bin/
+sudo chmod +x /usr/local/bin/docker-compose
+
+# remove docker
+# sudo apt-get remove docker docker-engine
+# sudo rm -fr /var/lib/docker/
 
 # Install vscode
 if [ ! -f /usr/bin/code ]; then
